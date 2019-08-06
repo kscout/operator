a3c310c872c811d55e0b32481a3e8a9596b845e4
Noah Huppert <contact@noahh.io>
2019-08-06 10:37:44 -0400

    added KScout kind and API

--- pkg/apis/kscout/v1/kscout_types.go
+++ pkg/apis/kscout/v1/kscout_types.go
@@ -4,23 +4,47 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// NOTE: json tags are required.  Any new fields you add must have json tags for
+// the fields to be serialized.
 
 // KScoutSpec defines the desired state of KScout
 // +k8s:openapi-gen=true
 type KScoutSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Important: Run "operator-sdk generate k8s" to regenerate code after
+	// modifying this file
+	// Add custom validation using kubebuilder tags:
+	// https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// MinReplicas is the lowest number of pods each service will be allowed
+	// to run.
+	MinReplicas int32 `json:"minReplicas"`
+
+	// CatalogAPI is the desired catalogue API state
+	CatalogAPI CatalogAPISpec `json:"catalogAPI"`
+}
+
+// CatalogAPISpec defines the desired state of the catalog API
+type CatalogAPISpec struct {
+	// ImageVersion is the container image tag
+	ImageVersion string `json:"imageVersion"`
 }
 
 // KScoutStatus defines the observed state of KScout
 // +k8s:openapi-gen=true
 type KScoutStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Important: Run "operator-sdk generate k8s" to regenerate code after
+	// modifying this file
+	// Add custom validation using kubebuilder tags:
+	// https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// CatalogAPI is the catalog API's status
+	CatalogAPI CatalogAPIStatus `json:"imageVersion"`
+}
+
+// CatalogAPIStatus defines the status of the catalog API
+type CatalogAPIStatus struct {
+	// ImageVersion is the container image tag
+	ImageVersion string `json:"imageVersion"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
